774377f94fec4b9431c28576f3fc95e16c6d06fd
rfernandezv <richar.fernandezv@gmail.com>
2018-12-14 19:11:58 -0500

    Subir

--- src/app/modules/product/list/list.component.ts
+++ src/app/modules/product/list/list.component.ts
@@ -48,8 +48,6 @@ export class ListComponentProduct implements OnInit {
   ngOnInit() {
       
       this.productDatabase = new ProductDataBase(this.httpClient, this.productService, this.messageAlertHandleService);
-
-      // If the user changes the sort order, reset back to the first page.
       this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 
       // Data
@@ -104,7 +102,7 @@ export class ListComponentProduct implements OnInit {
         });  
         dialogRef.afterClosed().subscribe(result => {
           if (result === 1) {
-            //this.changingData();    //rfv
+            this.changingData();
           }
         });
         
